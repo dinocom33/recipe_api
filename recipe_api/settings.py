43dd598d909d5853701a9a407552ec6c6ebe333d
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-10-30 16:51:27 +0200

    Added Github Actions

--- recipe_api/settings.py
+++ recipe_api/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'recipe_api.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DB_NAME', 'db'),
+        "NAME": os.environ.get('DB_NAME'),
         "USER": os.getenv('DB_USER'),
         "PASSWORD": os.getenv('DB_PASSWORD'),
         "HOST": os.getenv('DB_HOST'),
