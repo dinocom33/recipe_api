6ae4c12365fc7870f2ba88dcb74a15ff2cdaf8a4
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-10-31 17:49:54 +0200

    Added custom user model and tests for it

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin)
 
-# Create your models here.
+from core.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
--- recipe_api/settings.py
+++ recipe_api/settings.py
@@ -94,6 +94,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'core.User'
+
 # STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static_files'
 # STATICFILES_DIRS = [
