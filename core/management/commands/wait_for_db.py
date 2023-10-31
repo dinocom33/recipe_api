157f09615b7096c85a9e1eb14adec263aa1420d8
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-10-31 12:39:44 +0200

    Added wait_for_db command

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -4,6 +4,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
