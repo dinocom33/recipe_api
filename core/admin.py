c3b5827a96205dfe6074185abfcce07399a56a0c
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-11-01 12:19:44 +0200

    Added custom admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name', 'is_active', 'is_staff', 'is_superuser', 'last_login']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    list_filter = ['email', 'name', 'is_active', 'is_staff', 'is_superuser', 'last_login']
+    search_fields = ['email', 'name']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ from core.managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
