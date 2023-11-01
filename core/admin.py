c42c6cce473f0d90d8ec6b722637c814d941af80
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-11-01 12:33:58 +0200

    Added custom admin panel

--- core/admin.py
+++ core/admin.py
@@ -25,7 +25,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser'),
         }),
     )
 
