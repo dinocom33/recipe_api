74dc9b6f0d542218e154a705de98775fedab1149
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-11-03 16:34:07 +0200

    Added manage recipes functionality

--- recipe_api/urls.py
+++ recipe_api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('api/health-check/', core_views.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='api-schema'), name='redoc'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='api-schema'), name='api-redoc'),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
