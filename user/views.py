d7cbedc99071fb4938c4c3c15385cc57007f860e
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-11-03 10:55:48 +0200

    Added token API

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@
 Views for the User API.
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from user.serializers import UserSerializer, AuthTokenSerializer
 from rest_framework.settings import api_settings
@@ -19,3 +19,16 @@ class CreateTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+
+        return self.request.user
